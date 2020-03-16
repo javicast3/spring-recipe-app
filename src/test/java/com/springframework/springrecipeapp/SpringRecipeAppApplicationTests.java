b6a5907e48031c1c8045608b0334af69b8bb4b77
javicast3 <javicast3@gmail.com>
2020-03-16 17:44:50 -0300

    update master from app

--- src/test/java/com/springframework/springrecipeapp/SpringRecipeAppApplicationTests.java
+++ src/test/java/com/springframework/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringRecipeAppApplicationTests {
-
+	private String desc;
 	@Test
 	public void contextLoads() {
 	}
